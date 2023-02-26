34a92371a23aee23a65c67827edc870c658b01b1
laterdayi <122137647+laterdayi@users.noreply.github.com>
2023-02-26 10:10:57 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const registerNaiveUiTools = () => {
+  window.$message = useMessage();
+  window.$loadingBar = useLoadingBar();
+  window.$dialog = useDialog();
+  window.$notification = useNotification();
+};
+registerNaiveUiTools();
+</script>
+
+<template>
+  <router-view />
+</template>
